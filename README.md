fa9f982a6a0f1644f1b3d8dcef457c52ffbb1970
FabriceMk <fabricemk@users.noreply.github.com>
2017-12-09 15:31:50 +0900

    Initial commit with AspNetCore and ExpressJs targets

---
+++ README.md
@@ -0,0 +1,3 @@
+# Load Testing Toolbox
+
+This is my personal toolbox repository for load testing. It contains various load testing related stuff like custom scripts for some load testing frameworks and also a simple target API written with various Web Frameworks.
---
+++ benchmarking/targets/aspnet-core/Controllers/BenchmarkController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreTarget.Controllers
+{
+    [Route("[controller]")]
+    public class BenchmarkController : Controller
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello world!";
+        }
+    }
+}
