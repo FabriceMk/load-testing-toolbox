310636d55b8eda48289bf89f502b494db9dce3ea
FabriceMk <fabricemk@users.noreply.github.com>
2017-12-15 20:43:22 +0900

    Removes unnecessary using statements

--- benchmarking/targets/aspnet-core/Controllers/BenchmarkController.cs
+++ benchmarking/targets/aspnet-core/Controllers/BenchmarkController.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace AspNetCoreTarget.Controllers
 {
-    [Route("[controller]")]
+  [Route("[controller]")]
     public class BenchmarkController : Controller
     {
         [HttpGet]
